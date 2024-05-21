c8fbb713e1f222c24d18e6f05315a427bc095393
Carlos Tzompantzi <carlos.tz@wayssen.com.mx>
2024-05-20 19:40:43 -0600

    test 2

--- calculus/base.py
+++ calculus/base.py
@@ -3,13 +3,13 @@
 
 class Calculation:
     def add(self, a, b):
-        pass
+        return a + b
 
     def subtract(self, a, b):
-        pass
+        return a - b
 
     def multiply(self, a, b):
-        pass
+        return a * b
 
     def divide(self, a, b):
-        pass
+        return a // b
