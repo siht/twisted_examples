5db8c4ccddd17668a32b44ddc8619c87a39788ae
yo <yo@yo.yo>
2025-01-18 08:41:46 -0600

    finger02

--- finger/finger.py
+++ finger/finger.py
@@ -1,7 +1,17 @@
-from twisted.internet import reactor
+from twisted.internet import endpoints, protocol, reactor
+
+
+class FingerProtocol(protocol.Protocol):
+    pass
+
+
+class FingerFactory(protocol.ServerFactory):
+    protocol = FingerProtocol
 
 
 def main():
+    fingerEndpoint = endpoints.serverFromString(reactor, "tcp:1079")
+    fingerEndpoint.listen(FingerFactory())
     reactor.run()
 
 
