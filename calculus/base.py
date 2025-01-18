5724473ac14e79e7848a4fb5242301737c913f5d
yo <yo@yo.yo>
2025-01-17 22:54:58 -0600

    test 4 test_base

--- calculus/base.py
+++ calculus/base.py
@@ -1,15 +1,25 @@
-# -*- test-case-name: calculus.test.test_base_1 -*-
+# -*- test-case-name: calculus.test.test_base -*-
 
 
 class Calculation:
+    def _make_ints(self, *args):
+        try:
+            return [int(arg) for arg in args]
+        except ValueError:
+            raise TypeError("Couldn't coerce arguments to integers: {}".format(*args))
+
     def add(self, a, b):
+        a, b = self._make_ints(a, b)
         return a + b
 
     def subtract(self, a, b):
+        a, b = self._make_ints(a, b)
         return a - b
 
     def multiply(self, a, b):
+        a, b = self._make_ints(a, b)
         return a * b
 
     def divide(self, a, b):
+        a, b = self._make_ints(a, b)
         return a // b
--- calculus/test/test_base.py
+++ calculus/test/test_base.py
@@ -11,6 +11,12 @@ class CalculationTestCase(unittest.TestCase):
         result = operation(a, b)
         self.assertEqual(result, expected)
 
+    def _test_error(self, operation):
+        self.assertRaises(TypeError, operation, "foo", 2)
+        self.assertRaises(TypeError, operation, "bar", "egg")
+        self.assertRaises(TypeError, operation, [3], [8, 2])
+        self.assertRaises(TypeError, operation, {"e": 3}, {"r": "t"})
+
     def test_add(self):
         self._test(self.calc.add, 3, 8, 11)
 
@@ -22,3 +28,12 @@ class CalculationTestCase(unittest.TestCase):
 
     def test_divide(self):
         self._test(self.calc.divide, 12, 5, 2)
+
+    def test_errorAdd(self):
+        self._test_error(self.calc.add)
+
+    def test_errorMultiply(self):
+        self._test_error(self.calc.multiply)
+
+    def test_errorDivide(self):
+        self._test_error(self.calc.divide)
