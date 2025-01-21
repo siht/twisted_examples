24b726d2ba9acc2c0b8d88de7fe45aa9317ce49c
yo <yo@yo.yo>
2025-01-21 15:42:55 -0600

    update echo protocol

--- first_part/echo_protocols.py
+++ first_part/echo_protocols.py
@@ -2,7 +2,19 @@ from twisted.internet.protocol import Protocol
 
 
 class Echo(Protocol):
-    def dataReceived(self, data):
-        # lo que recibe como datos lo envía de vuelta
-        self.transport.write(data) # transport es una variable interna que sólo existe cuando hay un factory y un endpoint
+    def __init__(self, factory):
+        # esta clase se contruye cada vez que hay una conexion entrante, asi que se pierden los datos
+        self.factory = factory # hay que mantener una referencia al factory entonces donde vamos a guardar datos
 
+    def connectionMade(self):
+        # este metodo se llama automagicamente cuando se hace la conexion
+        self.factory.numProtocols = self.factory.numProtocols + 1
+        self.transport.write("Welcome! There are currently %d open connections.\n" % (self.factory.numProtocols,))
+
+    def connectionLost(self, reason):
+        # y este metodo es llamado cuando se desconecta
+        self.factory.numProtocols = self.factory.numProtocols - 1
+
+    def dataReceived(self, data):
+        # este cada vez que llegan datos
+        self.transport.write(data)
