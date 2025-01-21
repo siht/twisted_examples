971c9aa0ca322d083703afb376ae29fdc6c1de87
yo <yo@yo.yo>
2025-01-21 16:04:57 -0600

    simple protocol creation

--- first_part/factories.py
+++ first_part/factories.py
@@ -2,7 +2,7 @@ from twisted.internet.protocol import Factory
 from twisted.internet.endpoints import TCP4ServerEndpoint
 from twisted.internet import reactor
 
-from qotd_protocols import QOTD
+from qotd_protocols import QOTD, QOTDPersonalized
 
 
 class QOTDFactory(Factory):
@@ -10,10 +10,18 @@ class QOTDFactory(Factory):
         return QOTD() # es en este metodo que se puede regresar un protocolo muy personalizado
 
 
+class QOTDPersonalizedFactory(Factory):
+    # This will be used by the default buildProtocol to create new protocols:
+    protocol = QOTDPersonalized # no es necesario reescibir buildProtocol si no vas a hacer algo personalizado
+
+    def __init__(self, quote=None):
+        self.quote = quote or b'An apple a day keeps the doctor away' # se va a autoinyectar en el protocolo como self.factory.quote
+
+
 def main():
     # 8007 is the port you want to run under. Choose something >1024
     endpoint = TCP4ServerEndpoint(reactor, 8007)
-    endpoint.listen(QOTDFactory())
+    endpoint.listen(QOTDPersonalizedFactory(b"configurable quote"))
     reactor.run()
 
 
--- first_part/qotd_protocols.py
+++ first_part/qotd_protocols.py
@@ -6,3 +6,10 @@ class QOTD(Protocol):
     def connectionMade(self):
         self.transport.write(b"An apple a day keeps the doctor away\r\n")
         self.transport.loseConnection()
+
+
+class QOTDPersonalized(Protocol):
+    def connectionMade(self):
+        # self.factory was set by the factory's default buildProtocol:
+        self.transport.write(self.factory.quote + b'\r\n') # esto será añadido por el factory en la construccion
+        self.transport.loseConnection()
